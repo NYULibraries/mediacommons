cfae1faf81eb372708ce1aa77ddf15bff96a01b7
Alberto <aortiz@nyu.edu>
2012-12-10 00:49:00 -0500

    Use switch instead of IF in theme('button')

--- themes/subrubik/template.php
+++ themes/subrubik/template.php
@@ -3,31 +3,20 @@
 /** Preprocessor for theme('button') */
 function subrubik_preprocess_button(&$vars) {
   
-  if (isset($vars['element']['#name']) && $vars['element']['#name'] == 'field_contributed_piece_add_more') {
-    $vars['element']['#value'] = t('Add another contributed piece');
-  }
-  
-  if (isset($vars['element']['#name']) && $vars['element']['#name'] == 'field_contributors_add_more') {
-    $vars['element']['#value'] = t('Add another contributor');
-  }
-
-  if (isset($vars['element']['#value'])) {
-
-    $classes = array(
-      t('Save') => 'yes',
-      t('Submit') => 'yes',
-      t('Add') => 'yes',
-      t('Delete') => 'no',
-      t('Cancel') => 'no',
-    );
+  if (isset($vars['element']['#value']) && isset($vars['element']['#name'])) {
     
-    foreach ($classes as $search => $class) {
-      if (strpos($vars['element']['#value'], $search) !== FALSE) {
-        $vars['element']['#attributes']['class'][] = 'button-' . $class;
+    switch ($vars['element']['#name']) {
+      
+      case 'field_contributed_piece_add_more' :
+        $vars['element']['#value'] = t('Add another contributed piece');
+        break;      
+
+      case 'field_contributors_add_more' :
+        $vars['element']['#value'] = t('Add another contributor');
         break;
-      }
-    }
     
+    }
+
   }
   
 }
