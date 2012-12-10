5073a56e8a259c6b3d7d4cccda3494496655f449
Alberto Ortiz <aortiz@nyu.edu>
2012-12-10 18:36:21 -0500

    more stuff

--- themes/subrubik/template.php
+++ themes/subrubik/template.php
@@ -1,22 +1,3 @@
 <?php
 
 /** Preprocessor for theme('button') */
-function subrubik_preprocess_button(&$vars) {
-  
-  if (isset($vars['element']['#value']) && isset($vars['element']['#name'])) {
-    
-    switch ($vars['element']['#name']) {
-      
-      case 'field_contributed_piece_add_more' :
-        $vars['element']['#value'] = t('Add another contributed piece');
-        break;      
-
-      case 'field_contributors_add_more' :
-        $vars['element']['#value'] = t('Add another contributor');
-        break;
-    
-    }
-
-  }
-  
-}
