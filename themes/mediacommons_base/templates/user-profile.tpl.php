bc43b595d408cc4cbefd31eef1a2e68ac858ff16
Alberto <aortiz@nyu.edu>
2013-03-07 22:25:06 -0500

    Cleaning house

--- themes/mediacommons_base/templates/user-profile.tpl.php
+++
@@ -1,3 +0,0 @@
-<div class="profile"<?php print $attributes; ?>>
-  <?php print $safe_user_profile; ?>
-</div>
--- themes/mediacommons_profiles/theme-settings.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-/** function mediacommons_profile_form_system_theme_settings_alter(&$form, &$form_state) { } */
