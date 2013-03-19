0ad8c186fbfe240b15ecbab389070998d667234c
Mark Reilly <mark@alienresident.net>
2013-03-19 16:07:47 -0400

    Cleaned up the script still not 100% there

--- import-scripts/import-hubs.php
+++ import-scripts/import-hubs.php
@@ -8,14 +8,13 @@
 // module_load_include('inc', 'node', 'node.pages');
 
 define('DRUPAL_ROOT', getcwd());
-include_once 
-
-DRUPAL_ROOT . '/includes/bootstrap.inc';
+$_SERVER['REMOTE_ADDR'] = "localhost"; // Necessary if running from command line
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 bootstrap_invoke_all('init');
 ini_set('memory_limit', '512M');
 //Authenticate as user 1
-user_authenticate('user', 'password');
+//user_authenticate('user', 'password');
 module_load_include('inc', 'node', 'node.pages');  // new for Drupal 6
 $row = 0;
 $header = NULL;
@@ -34,58 +33,59 @@ if (($handle = fopen("/Applications/MAMP/htdocs/mcd7temp/mediacommons/import-scr
   $num = count($data);
   for ($c=0; $c < $num; $c++) {
     $node = new StdClass();
-    
-    
-    //$node->nid = $data[$c]['nid'];
-    
+    //$node = node_load($nid);
     $node->type = $data[$c]['type'];
-    $node->language = LANGUAGE_NONE;
+    $node->language = $data[$c]['language'];
+    $node->status = $data[$c]['status'];
+    $node->promote = 0;
+    $node->sticky = 0;
     node_object_prepare($node);
     
+    $node->title = $data[$c]['title'];
+    $node->uid = $data[$c]['uid'];
     
-    $form_state = array();
-    $form_state['values']['status'] = $data[$c]['status'];
-    $form_state['values']['title'] = $data[$c]['title'];   // the node's title
-    $form_state['values']['body'] = 'just my test node'; // the body, not required
+    $node->date = (int)$data[$c]['created'];
+    //$node->changed = $data[$c]['changed'];
+    $node->comment = $data[$c]['comment'];
+    // contributors
     $array = explode(", ", $data[$c]['contributors']);
     foreach ($array as $key => $value) {
-      # code...
-
-    $form_state['values']['field_contributors'][$node->language][$key]['uid'] = (int)$value;
-    //$form_state['values']['field_contributors'][$node->language][$key]['_weight'] = $key;
-    } 
-    //$form_state['values']['promote'] = 1; //promote all imported nodes
-    $form_state['values']['sticky'] = 0; //remove sticky from imported nodes
-    //$form_state['values']['image'] = array();
-    //$form_state['values']['name'] = 'reilly';
-    $form_state['values']['uid'] = $data[$c]['uid'];
-    $form_state['values']['op'] = t('Save');  // this seems to be a required value
-    var_dump($form_state);
-    
-    drupal_form_submit("{$node->type}_node_form", $form_state, $node);
-    var_dump($node);
-    //$node->title = $data[$c]['title'];
-    // $node->uid = $data[$c]['uid'];
-    //   $node->status = $data[$c]['status'];
-    //   $node->created = $data[$c]['created'];
-    //   $node->changed = $data[$c]['changed'];
-    //   $node->comment = $data[$c]['comment'];
-    //$node->field_contributors[$node->language][]['value'] = $data[$c]['contributors'];
-    //$node->field_response[0]['value'] = $data[$c]['field_response_value'];
-    //$node->field_response[0]['format'] = $data[$c]['field_response_format'];
-    //$node->field_sr_reference[0]['nid']  = $data[$c]['field_sr_reference_nid'];
+      $node->field_contributors[$node->language][$key]['uid'] = (int)$value;
+    }
+    // contributor_order
+    $array = explode(", ", $data[$c]['contributor_order']);
+    foreach ($array as $key => $value) {
+      $node->field_contributors[$node->language][$key]['_weight'] = (int)$value;
+    }
+    $node->field_description[$node->language][0]['value'] = $data[$c]['field_description_value'];
+    $node->field_description[$node->language][0]['format'] = $data[$c]['field_description_format'];
+    $node->field_type[$node->language][0]['value'] = $data[$c]['field_type_value'];
+    $node->field_video_embed_link[$node->language][0]['video_url'] = $data[$c]['field_video_embed_link_video_url'];
+    $node->field_representative_image_fid[$node->language][0]['fid'] = $data[$c]['field_representative_image_fid'];
+    // spokes
+    $array = explode(", ", $data[$c]['contributed_pieces']);
+    foreach ($array as $key => $value) {
+      $node->field_spokes[$node->language][$key]['target_id'] = (int)$value;
+    }
+    // spoke_order
+    $array = explode(", ", $data[$c]['contributed_pieces_order']);
+    foreach ($array as $key => $value) {
+      $node->field_spokes[$node->language][$key]['_weight'] = (int)$value;
+    }
+    $node->field_field_period[$node->language][0]['value'] = $data[$c]['field_field_period_value'];
+    $node->field_field_period[$node->language][0]['value2'] = $data[$c]['field_field_period_value2'];
     /*if(!empty($data[$c]['filename'])){
     //$file = field_file_save_file('/Users/mar22/Dropbox/NYU/MediaCommons/redesign/Frontpage-tweaks-2013/content/images/'.$data[$c]['filename'], array(), 'files');
     $file = field_file_save_file('/www/sites/mediacommons/script-tmp/images/'.$data[$c]['filename'], array(), 'files/front_page_images');
     $node->field_resp_image = array($file);
   }*/
-      // if($node = node_submit($node)) { // Prepare node for saving
-      //                                                     node_save($node);
-      //                                                     echo "Node with nid " . $node->nid . " saved!\n";
-                                    //}
-      // node_save($node);
-      // print_r($node);
-
+  var_dump($node->date);
+      if($node = node_submit($node)) { // Prepare node for saving
+        var_dump($node->created);
+        node_save($node);
+        echo "Node with nid " . $node->nid . " saved!\n";
+        }
+        
      }
      fclose($handle);
   }
