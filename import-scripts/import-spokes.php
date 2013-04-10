0dc2d15db255539970b00da4a456e6a913ab7619
Mark Reilly <mark@alienresident.net>
2013-04-10 12:18:17 -0400

    Latest code

--- import-scripts/import-spokes.php
+++ import-scripts/import-spokes.php
@@ -35,30 +35,32 @@ if (($handle = fopen($script_path . $csv_file, "r")) !== FALSE) {
     node_object_prepare($node);
     $node->title = $data[$c]['title'];
     $node->uid = (int)$data[$c]['uid'];
-    //$node->created = (int)$data[$c]['created'];
-    //$node->changed = $data[$c]['changed'];
+    $node->created = (int)$data[$c]['created'];
+    $node->changed = $data[$c]['changed'];
     $node->comment = $data[$c]['comment'];
+    
     // additonal_authors
+    
     if(($data[$c]['additonal_authors'] != 'NULL')) {
-      $array = explode(", ", $data[$c]['additonal_authors']);
-      foreach ($array as $key => $value) {
-        $node->field_contributors[$node->language][$key]['uid'] = (int)$value;
-      }
-    }
+          $array = explode(", ", $data[$c]['additonal_authors']);
+          foreach ($array as $key => $value) {
+            $node->field_contributors[$node->language][$key]['uid'] = (int)$value;
+            
+          }
+        }
+    
     // cluster_nid
+    
     if(($data[$c]['cluster_nid'] != 'NULL')) {
-      $array = explode(", ", $data[$c]['cluster_nid']);
-      foreach ($array as $key => $value) {
-        $node->field_part_of_hub[$node->language][$key]['nid'] = (int)$value;
-      }
+      //$node->field_part_of_hub[$node->language][$key]['nid'] = (int)$data[$c]['cluster_nid'];
     }
     // description
     $node->field_body[$node->language][0]['value'] = $data[$c]['field_document_textarea_body_value'];
     $node->field_body[$node->language][0]['format'] = 'filtered_html';
     // publication status
-    if(($data[$c]['field_video_embed_link_embed'] != 'NULL') || ($data[$c]['field_pubstat_value'] == 'Published')) {
+    if($data[$c]['field_pubstat_value'] == 'Published') {
       $node->field_pubstat[$node->language][0]['value'] = 1;
-    } elseif(($data[$c]['field_video_embed_link_embed'] != 'NULL') || ($data[$c]['field_pubstat_value'] == 'Unpublished'))  {
+    } elseif($data[$c]['field_pubstat_value'] == 'Unpublished')  {
       $node->field_pubstat[$node->language][0]['value'] = 0;
     }
     //representative image
@@ -67,28 +69,40 @@ if (($handle = fopen($script_path . $csv_file, "r")) !== FALSE) {
       $file_path = $script_path . $image_source . $data[$c]['filename'];
       
       $file = (object) array(
-              'uid' => (int)$data[$c]['uid'],
-              'uri' => $file_path,
-              'filemime' => file_get_mimetype($file_path),
-              'status' => 1,
-            ); 
-      $file = file_copy($file, 'public://'. $image_destination); // Save the file to the root of the files directory. 
-      $node->field_representative_image[$node->language][0] = (array)$file; //associate the file object with the image field:
+                   'uid' => (int)$data[$c]['uid'],
+                   'uri' => $file_path,
+                   'filemime' => file_get_mimetype($file_path),
+                   'status' => 1,
+                 ); 
+           $file = file_copy($file, 'public://'. $image_destination); // Save the file to the root of the files directory. 
+           
+           // ALBERTO I'm having trouble loading the file into the attached image table
+           
+           //$node->field_attached_images[$node->language][0] = (array)$file; //associate the file object with the image field:
+           $node->field_representative_image[$node->language][0] = (array)$file; //associate the file object with the image field:
+     
     }
-    // spokes
-    $array = explode(", ", $data[$c]['contributed_pieces']);
-    foreach ($array as $key => $value) {
-      $node->field_spokes[$node->language][$key]['target_id'] = (int)$value;
-    }
-    // spoke_order
-    $array = explode(", ", $data[$c]['contributed_pieces_order']);
-    foreach ($array as $key => $value) {
-      $node->field_spokes[$node->language][$key]['_weight'] = (int)$value;
+    //attached images
+    if(($data[$c]['attached_image_filepath'] != 'NULL')){
+      $array = explode(", ", $data[$c]['attached_image_filepath']);
+      foreach ($array as $key => $value) {
+      $filename = pathinfo($value, PATHINFO_BASENAME);
+      $file_path = $script_path . $image_source . $filename;
+      $file = (object) array(
+                    'uid' => (int)$data[$c]['uid'],
+                    'uri' => $file_path,
+                    'filemime' => file_get_mimetype($file_path),
+                    'status' => 1,
+                  ); 
+           $file = file_copy($file, 'public://'. $image_destination); // Save the file to the root of the files directory. 
+            //$node->field_attached_images[$node->language][0] = (array)$file; //associate the file object with the image field:
+      
+      }
     }
     // taxonomy
     $array = explode(", ", $data[$c]['terms']);
     foreach ($array as $key => $value) {
-      $node->field_tags[$node->language][$key]['tid'] = (int)$value;
+      //$node->field_tags[$node->language][$key]['tid'] = (int)$value;
     }
     
     if($node = node_submit($node)) { // Prepare node for saving
@@ -100,9 +114,10 @@ if (($handle = fopen($script_path . $csv_file, "r")) !== FALSE) {
   fclose($handle);
 }
 
-/* 
+/*
 Read more: http://zengenuity.com/blog/a/201011/importing-files-drupal-nodes-cck-fieldfield-imagefield#ixzz1v3tz75My
 http://fooninja.net/2011/04/13/guide-to-programmatic-node-creation-in-drupal-7/
 http://drupal.org/node/330421#comment-2806336
 http://www.group42.ca/creating_and_updating_nodes_programmatically_in_drupal_7
+http://timonweb.com/how-programmatically-create-nodes-comments-and-taxonomies-drupal-7
 */
