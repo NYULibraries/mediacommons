3e5ae26d06382bd4b9960350d0b7f9585385dd41
Alberto Ortiz Flores <dismorfo@gmail.com>
2013-12-13 02:56:14 -0500

    script now accept a configuration file as argument

--- scripts/deploy_build.sh
+++ scripts/deploy_build.sh
@@ -5,8 +5,6 @@ die () {
   exit 1
 }
 
-[ "$#" -eq 1 ] || die "Please provide make file"
-
 SOURCE="${BASH_SOURCE[0]}"
 
 # resolve $SOURCE until the file is no longer a symlink
@@ -20,21 +18,76 @@ done
 
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-LIBRARY="$(dirname "$DIR")"/lib
+CONF_FILE=$DIR/../project.conf   
 
-. $DIR/../project.conf
+MAKE_FILE=$1
 
-[ -f $1 ] || die "File $1 does not exist"
+while test $# -gt 0; do
 
-[ -d $LIBRARY ] || die "Library directory $LIBRARY does not exist"
+  case "$1" in
+
+    -h|--help)
+      echo " "
+      echo "usage: sh deploy_build.sh [options] example.make"
+      echo " "
+      echo "options:"
+      echo "-h, --help                          show brief help"
+      echo "-c, --configuration=example.conf    specify the configuration file to use"
+      exit 0
+    ;;
+    
+    -c|--configuration)
+      CONF_FILE=$2
+      MAKE_FILE=$3
+      shift
+    ;;
+    
+    *)
+      break
+    ;;
+
+  esac
+done
+
+[ -f $CONF_FILE ] || die "Configuration file does not exist"
+
+[ -f $MAKE_FILE ] || die "Please provide make file"
+
+CONF_PERM=`ls -l $CONF_FILE | awk '{print $1}'`
+
+# make this a bit more robust
+if [ "$CONF_PERM" != "-rwx------@" ] 
+  then 
+    echo "Please change configuration file permission to be read only by owner" 
+fi
+
+. $CONF_FILE
 
 [ -d $BUILD_DIR ] || die "Build directory $BUILD_DIR does not exist" 
 
-echo Preparing new site using $1
-drush -v make --prepare-install $1 $BUILD_DIR/$BUILD_NAME
-  
-echo Install new site
-cd $BUILD_DIR/$BUILD_NAME
-drush -v site-install $DRUPAL_INSTALL_PROFILE_NAME --site-name=$DRUPAL_SITE_NAME --account-name=$DRUPAL_ACCOUNT_NAME --account-mail=$DRUPAL_ACCOUNT_MAIL --site-mail=$DRUPAL_SITE_MAIL --db-url=$DRUPAL_SITE_DB_TYPE://$DRUPAL_SITE_DB_USER:$DRUPAL_SITE_DB_PASS@$DRUPAL_SITE_DB_ADDRESS/$DRUPAL_DB_NAME
+echo Preparing new site using $MAKE_FILE
 
-echo Ok
+drush -v make --prepare-install $MAKE_FILE $BUILD_DIR/$BUILD_NAME
+
+[ -d $BUILD_DIR/$BUILD_NAME ] || die "Unable to install new site. Build does not exist" 
+
+echo Install new site
+
+cd $BUILD_DIR/$BUILD_NAME
+
+drush -v site-install $DRUPAL_INSTALL_PROFILE_NAME --site-name="$DRUPAL_SITE_NAME" --account-name=$DRUPAL_ACCOUNT_NAME --account-mail=$DRUPAL_ACCOUNT_MAIL --site-mail=$DRUPAL_SITE_MAIL --db-url=$DRUPAL_SITE_DB_TYPE://$DRUPAL_SITE_DB_USER:$DRUPAL_SITE_DB_PASS@$DRUPAL_SITE_DB_ADDRESS/$DRUPAL_DB_NAME
+
+cd $BUILD_DIR/$BUILD_NAME/sites/all/themes/mediacommons_base
+
+echo Compile CSS
+
+compass compile --force .
+
+# Test if site is up and running
+
+STATUS=`curl -s -o /dev/null -w "%{http_code}" $BASE_URL`
+
+if [ "$STATUS" != "404" ] 
+  then 
+  echo Build done. Status report $STATUS
+fi
