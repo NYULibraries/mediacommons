a3ff13f99b3f1d1f17b6eecbbaa0518aa8f6b76d
Alberto Ortiz Flores <aortiz@nyu.edu>
2015-01-15 07:14:10 -0500

    Get the latest mediacommons.make file

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# make sure the most recent changes to *.make file are in place
+
+rm mediacommons.make
+
+wget https://raw.githubusercontent.com/NYULibraries/mediacommons/master/mediacommons.make
+
 # Takes care of grabbing the most recent production databases and inport them 
 ./scripts/prepare.sh
 
