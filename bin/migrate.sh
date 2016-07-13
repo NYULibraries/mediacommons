e40c8eb0dc85843de4b6c03a0cf6c8779ead8df5
Alberto Ortiz Flores <aortiz@nyu.edu>
2016-07-13 17:16:28 -0400

    update migrate.sh

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -101,7 +101,7 @@ if [[ -f $BUILD_DIR/$BUILD_BASE_NAME/index.php ]]; then
     if [[ $BUILD_BASE_NAME != *"intransition"* ]] && [[ $BUILD_BASE_NAME != *"imr"* ]] 
       then
       DBSTRING+="'vocabulary' => '"$D6_SHARED"."$D6_SHARED_DB_PREFIX"',"
-      DBSTRING+="'term_data' => '"$D6_SHARED"."$D6_SHARED_DB_PREFIX"',"      
+      DBSTRING+="'term_data' => '"$D6_SHARED"."$D6_SHARED_DB_PREFIX"',"
     fi
     DBSTRING+="),"
     DBSTRING+="),"
@@ -158,6 +158,10 @@ if [[ -f $BUILD_DIR/$BUILD_BASE_NAME/index.php ]]; then
 
     # Append database set-up to settings.php file
     echo $DBSTRING >> $BUILD_DIR/$BUILD_BASE_NAME/sites/default/settings.php
+
+    $SHARE_STRING="$databases['default']['default']['prefix'] = array ('default' => '', 'mediacommons_base_import_vocabulary_map' => 'shared.', 'mediacommons_base_import_term_map' => 'shared.', 'taxonomy_vocabulary' => 'shared.', 'taxonomy_term_data' => 'shared.', 'taxonomy_term_hierarchy' => 'shared.', );"
+
+    echo $SHARE_STRING >> $BUILD_DIR/$BUILD_BASE_NAME/sites/default/settings.php
 
     # we have what it looks like a Drupal site
     # test if we can connect to the database and login as user 1
