e2dc7cd5ce9ad68465f5b1fbefab701b1a644344
David <da70@nyu.edu>
2016-07-21 15:37:11 -0400

    refresh-from-dev.sh: use options instead of command line arguments for specifying parameter values; add new param "expect" mode

--- bin/refresh-from-dev_common.sh
+++ bin/refresh-from-dev_common.sh
@@ -7,16 +7,16 @@ DEV_SERVER_DATABASE_DUMPS=/www/sites/drupal/scripts/mediacommons/builds/dbs/7
 DEV_SERVER_FILES=/content/dev/pa/drupal/mediacommons/7
 
 # Directory where storing local copies of devmc database dumps
-DATABASE_DUMPS=$1
+DATABASE_DUMPS=
 
 # Directory where storing local copies of devmc files/
-MC_FILES=$2
+MC_FILES=
 
 # Username on dev server
-DEV_SERVER_USERNAME=$3
-if [ -z $DEV_SERVER_USERNAME ]; then
-    DEV_SERVER_USERNAME=$(whoami)
-fi
+DEV_SERVER_USERNAME=
+
+# Running using `expect` script?
+EXPECT_MODE=false
 
 # Mediacommons main site and channel sites
 declare -a ALL_SITES=( alt-ac fieldguide imr intransition mediacommons tne )
@@ -31,16 +31,27 @@ function usage() {
 
     cat <<EOF
 
-usage: ${script_name} DATABASE_DUMPS MC_FILES [DEV_SERVER_USERNAME]
+usage: ${script_name} -d DATABASE_DUMPS [-e] -f MC_FILES [-u DEV_SERVER_USERNAME]
+    options:
+        -d DATABASE_DUMPS       Full path to local directory where database dumps
+                                    will be stored.
+        -e                      "expect" mode.  Indicates this script is being run from
+                                    the expect wrapper script.
+        -f MC_FILES             Full path to local directory where Drupal files/
+                                    directories will be stored.
+        -u DEV_SERVER_USERNAME  Optional username to use when logging into the dev
+                                    server.  Defaults to \$(whoami).
 
 examples:
 
     # Specify user name on dev server
-    ./${script_name} ~/mediacommons_databases ~/mediacommons_files somebody
+    ./${script_name} -d ~/mediacommons_databases -f ~/mediacommons_files -u somebody
 
     # Use user name on local machine as login name on dev server
-    ./${script_name} ~/mediacommons_databases ~/mediacommons_files
+    ./${script_name} -d ~/mediacommons_databases -f ~/mediacommons_files
 
+    # Call from expect script, which automates the interactions with rsync.
+    ./${script_name} -d ~/mediacommons_databases -f ~/mediacommons_files -e
 EOF
 }
 
@@ -90,4 +101,26 @@ function select_sites() {
     echo "Selected sites to refresh: ${selected_sites[*]}"
 }
 
+while getopts d:ef:u: opt
+do
+    case $opt in
+        d) DATABASE_DUMPS=$OPTARG ;;
+        e) EXPECT_MODE=true ;;
+        f) MC_FILES=$OPTARG ;;
+        u) DEV_SERVER_USERNAME=$OPTARG ;;
+        *) echo >&2 "Options not set correctly."; usage; exit 1 ;;
+    esac
+done
+
+if [ -z $DEV_SERVER_USERNAME ]; then
+    DEV_SERVER_USERNAME=$(whoami)
+fi
+
 validate_args
+
+echo $DATABASE_DUMPS
+echo $MC_FILES
+echo $DEV_SERVER_USERNAME
+echo $EXPECT_MODE
+
+exit
