9a51fe99b3312a4f134738e3968e84aff9fa1c5f
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-12-07 20:39:15 -0500

    bin/autobuild.sh

--- bin/autobuild.sh
+++ bin/autobuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 die () {
   echo "file: ${0} | line: ${1} | step: ${2} | message: ${3}";
   if [[ -f ${TEMP_DIR}/autobuild.pid ]]; then
@@ -82,38 +80,41 @@ if [ "$UPDATE" = true ];
 fi
 
 # Build and migrate Umbrella before anything else
-${BUILD_APP_ROOT}/bin/umbrella.sh -c ${BUILD_APP_ROOT}/configs/build.conf -m ${MAKE_FILE} ${SKIP};
+${BUILD_APP_ROOT}/bin/umbrella.sh -c ${BUILD_APP_ROOT}/configs/build.conf -m ${MAKE_FILE} ${SKIP}
 
 projects=(${PROJECTS})
 
 for project in ${projects[*]}
   do
+    echo ${project}
     # -l run in legacy mode
     # -e environment we are building
     # -k use cookies to share databases
-    ${BUILD_APP_ROOT}/bin/build.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf -m ${MAKE_FILE} -e ${ENVIRONMENT} -k;
+    ${BUILD_APP_ROOT}/bin/build.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf -m ${MAKE_FILE} -e ${ENVIRONMENT} -k -bg ${BUILD_APP_ROOT}/configs/build.conf
     if [ $? -eq 0 ];
       then
-        echo "Successful: Build ${project}";
+        echo "Successful: Build ${project}"
         # Run preprocess task
-        echo "Run preprocess task";
-        ${BUILD_APP_ROOT}/bin/utilities/preprocess.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf;
+        echo "Run preprocess task"
+        ${BUILD_APP_ROOT}/bin/utilities/preprocess.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf
         # Migrate the content
-        echo "Migrate the content";
-        ${BUILD_APP_ROOT}/bin/migrate.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf;
-        echo "Set-up and clean-up others";
-        ${BUILD_APP_ROOT}/bin/utilities/postprocess.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf;
-        echo "Set-up Apache Solr";
-        ${BUILD_APP_ROOT}/bin/utilities/apachesolr.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf;
+        echo "Migrate the content"
+        ${BUILD_APP_ROOT}/bin/migrate.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf
+        echo "Set-up and clean-up others"
+        ${BUILD_APP_ROOT}/bin/utilities/postprocess.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf
+        echo "Set-up Apache Solr"
+        ${BUILD_APP_ROOT}/bin/utilities/apachesolr.sh -c ${BUILD_APP_ROOT}/configs/${project}.conf
       else
-        echo ${LINENO} "build" "Fail: Build ${project}";
-    fi;
-done;
+        echo ${LINENO} "build" "Fail: Build ${project}"
+    fi
+done
 
-${BUILD_APP_ROOT}/bin/utilities/mcblocks.sh -c ${BUILD_APP_ROOT}/configs/build.conf;
+${BUILD_APP_ROOT}/bin/utilities/solr.sh -c ${BUILD_APP_ROOT}/configs/build.conf
+
+${BUILD_APP_ROOT}/bin/utilities/mcblocks.sh -c ${BUILD_APP_ROOT}/configs/build.conf
 
 if [[ -f ${TEMP_DIR}/autobuild.pid ]]; then
-  rm -f ${TEMP_DIR}/autobuild.pid;
+  rm -f ${TEMP_DIR}/autobuild.pid
 fi
 
-exit 0;
+exit 0
