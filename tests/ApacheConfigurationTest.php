2d0b84236817e9f0556379cf3b717d0b4a864fe2
David <da70@nyu.edu>
2018-03-21 16:51:12 -0400

    tests/ApacheConfigurationTest.php: new method generateTestUrls()

--- tests/ApacheConfigurationTest.php
+++ tests/ApacheConfigurationTest.php
@@ -2,6 +2,21 @@
 use PHPUnit\Framework\TestCase;
 
 final class ApacheConfigurationTest extends TestCase {
+    const CANONICAL_PARENT_DOMAIN_NAME = 'mediacommons.org';
+    const INSTANCES = [
+        'dev' => [
+            'basename' => 'dev',
+        ],
+        'stage' => [
+            'basename' => 'stage',
+        ],
+        'prod' => [
+            'basename' => '',
+        ]
+    ];
+    const PARENT_DOMAINS = [ 'mediacommons.org', 'media-commons.org' ];
+    const PATHS = [ '', 'alt-ac', 'fieldguide', 'imr', 'intransition', 'mediacommons', 'tne' ];
+    const PREFIXES = [ 'www', '' ];
 
     public function testTest() {
         $startUrl = "http://dev.media-commons.org/mediacommons/";
@@ -12,6 +27,59 @@ final class ApacheConfigurationTest extends TestCase {
 
         $this->assertEquals( $expectedEndUrl, $effectiveUrl );
         $this->assertEquals( $expectedNumRedirects, $numRedirections);
+    }
+
+    public function generateTestUrls() {
+        $testUrls = [];
+
+        foreach ( self::INSTANCES as $instance ) {
+            $basename = $instance[ 'basename' ];
+
+            foreach(self::PARENT_DOMAINS as $parentDomain ) {
+
+                foreach( self::PREFIXES as $prefix ) {
+                    $subdomain = $basename;
+
+                    if ( $prefix ) {
+                        if ( $subdomain ) {
+                            $subdomain = $prefix . '-' . $subdomain;
+                        } else {
+                            $subdomain = $prefix;
+                        }
+                    } else {
+                        // Do nothing
+                    }
+
+                    foreach (self::PATHS as $path ) {
+
+                        if ( $subdomain ) {
+                            $canonicalFullyQualifiedDomainName = $basename . '.' . self::CANONICAL_PARENT_DOMAIN_NAME;
+                            $fullyQualifiedDomainName = "${subdomain}.${parentDomain}";
+                        } else {
+                            $canonicalFullyQualifiedDomainName = self::CANONICAL_PARENT_DOMAIN_NAME;
+                            $fullyQualifiedDomainName = "${parentDomain}";
+                        }
+
+                        if ( $path ) {
+                            $canonicalUrl = $canonicalFullyQualifiedDomainName . "/${path}/";
+
+                            $testUrls[ "${fullyQualifiedDomainName}/${path}" ] = $canonicalUrl;
+                            $testUrls[ "${fullyQualifiedDomainName}/${path}/" ] = $canonicalUrl;
+                        } else {
+                            $canonicalUrl = $canonicalFullyQualifiedDomainName . '/';
+
+                            $testUrls[ "${fullyQualifiedDomainName}" ] = $canonicalUrl;
+                            $testUrls["${fullyQualifiedDomainName}/" ] = $canonicalUrl;
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
+        return $testUrls;
     }
 
     public function checkRedirect( $url ) {
