9cad8e4fb05c1dd382aceb21001eb5b40c9862f3
Alberto Ortiz Flores <dismorfo@gmail.com>
2018-05-11 14:07:51 -0400

    autobuild.sh

--- bin/autobuild.sh
+++ bin/autobuild.sh
@@ -108,6 +108,8 @@ done
 
 ${BUILD_APP_ROOT}/bin/utilities/solr.sh -c ${BUILD_APP_ROOT}/configs/build.conf
 
+${BUILD_APP_ROOT}/bin/utilities/cron.sh -c ${BUILD_APP_ROOT}/configs/build.conf
+
 ${BUILD_APP_ROOT}/bin/utilities/mcblocks.sh -c ${BUILD_APP_ROOT}/configs/build.conf
 
 ${BUILD_APP_ROOT}/bin/utilities/patches.sh -c ${BUILD_APP_ROOT}/configs/build.conf
